27790bfce9ac4262b865ffeb1e7e31284c1bee85
ryyano <yato2001@gmail.com>
2024-07-18 21:08:13 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MapComponent from "./utils/MapComponent";
+
+function App() {
+  return (
+    <>
+      <h1>photo-map</h1>
+      <div className="App">
+        <MapComponent />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/MapComponent.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react';
+
+const MapComponent = () => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const loadGoogleMapsScript = () => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=(Constants.apiKey)`;
+      script.async = true;
+      script.defer = true;
+      script.onload = initMap;
+      document.head.appendChild(script);
+    };
+
+    const initMap = () => {
+      const map = new window.google.maps.Map(mapRef.current, {
+        center: { lat: 35.6586, lng: 139.7454 },
+        zoom: 15,
+      });
+
+      new window.google.maps.Marker({
+        position: { lat: 35.6586, lng: 139.7454 },
+        map,
+        title: "Hello World!",
+      });
+
+      map.addListener('rightclick', (event) => {
+        const latLng = event.latLng;
+        // 右クリックイベントの処理
+        new window.google.maps.Marker({
+          position: { lat: latLng.lat(), lng: latLng.lng() },
+          map,
+          title: "Hello World!",
+        });
+      });
+    };
+
+    loadGoogleMapsScript();
+  }, []);
+
+  return (
+    <div
+      ref={mapRef}
+      style={{ width: '100%', height: '800px' }}
+    >
+    </div>
+  );
+};
+
+export default MapComponent;
