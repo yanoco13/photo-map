1f777f241b56c220efa3f70a4228c60b2d356891
ryyano <yato2001@gmail.com>
2024-07-26 20:51:00 +0900

    モーダル表示

---
+++ src/utils/RegistSpot.js
@@ -0,0 +1,13 @@
+import './ResgistSpot.css'
+
+const RegistSpot = () => {
+    return(
+        <>
+            <div id = "overlay">
+                <h1>モーダル</h1>
+            </div>
+        </>
+    )
+}
+
+export default RegistSpot;
